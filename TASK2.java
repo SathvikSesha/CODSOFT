319da97b42518bbbc056ce2e8a703efc4679b2b2
SathvikSesha <lakshmisarmatk@gmail.com>
2024-12-26 11:04:31 +0530

    Final commit

--- TASK2.java
+++ TASK2.java
@@ -1,3 +1,4 @@
+package CODSOFT;
 import java.util.Scanner;
 public class TASK2 {
     public static char calcGrade(double percentage){
@@ -26,16 +27,16 @@ public class TASK2 {
         System.out.print("Enter the number of subjects:");
         int n = sc.nextInt();
         if(n<=0){
-            System.out.println("Number of subjects must be greater than 0.");
+            System.out.println("Number of subjects must be greater than 0");
             return;
         }
         double[] marks=new double[n];
         double totalMarks = 0;
         for(int i=0;i<n;i++){
-            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            System.out.print("Enter marks obtained in subject "+(i + 1)+" (out of 100): ");
             double mark = sc.nextDouble();
             if (mark<0 || mark>100){
-                System.out.println("Marks should be in the range of 0 to 100.");
+                System.out.println("Marks should be in the range of 0 to 100");
                 return;
             }
             marks[i]=mark;
